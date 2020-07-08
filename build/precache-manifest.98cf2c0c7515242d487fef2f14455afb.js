dec4e75c81f781c9a3863dac297d8f0708608148
JackieCalapristi <jackie.lc11@gmail.com>
2020-07-08 15:32:37 -0700

    Saying  an extra hail mary

---
+++ build/precache-manifest.98cf2c0c7515242d487fef2f14455afb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaffcf7a662d463f182d705b4c0ac28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6fbdb66de050a24545",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3437e20802315fc76d97",
+    "url": "/static/js/2.4b352f18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b352f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6fbdb66de050a24545",
+    "url": "/static/js/main.46171ecb.chunk.js"
+  },
+  {
+    "revision": "2ea8365bf67639f37732",
+    "url": "/static/js/runtime-main.7af8a1c4.js"
+  },
+  {
+    "revision": "949e3d0bcae997b35b6cf6940f68a4bd",
+    "url": "/static/media/BackgroundDesk.949e3d0b.jpg"
+  },
+  {
+    "revision": "89995abbafcd5bd690e9280a3f925cdd",
+    "url": "/static/media/Calendar.89995abb.svg"
+  },
+  {
+    "revision": "0790af74c7a71794746534427e6e1c4a",
+    "url": "/static/media/Conversica.0790af74.png"
+  },
+  {
+    "revision": "6458fa4965a76a293f008370284bcaaa",
+    "url": "/static/media/Gatsby.6458fa49.png"
+  },
+  {
+    "revision": "a08aabd3671f19999496535740a3af33",
+    "url": "/static/media/Headshot.a08aabd3.jpg"
+  },
+  {
+    "revision": "4ab8102eba7a176e884784f700332462",
+    "url": "/static/media/Jackie_Calapristi_Resume_2020.4ab8102e.pdf"
+  },
+  {
+    "revision": "b490139690a9f5abb069777dae3524f0",
+    "url": "/static/media/Luum.b4901396.png"
+  },
+  {
+    "revision": "95a6b5b148dc2a74e83392826e484ed0",
+    "url": "/static/media/Muli-Black.95a6b5b1.woff"
+  },
+  {
+    "revision": "c50e615d57194d12132252bd916df815",
+    "url": "/static/media/Muli-ExtraLight.c50e615d.ttf"
+  },
+  {
+    "revision": "aae50cb40468371ec64eb70d1a50fba8",
+    "url": "/static/media/Muli-Regular.aae50cb4.woff"
+  },
+  {
+    "revision": "46818ebd4f76c4e6fe9b030dbc74f5cf",
+    "url": "/static/media/Raleway-Black.46818ebd.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/static/media/Raleway-Medium.bb5ae98e.ttf"
+  },
+  {
+    "revision": "a43fdae0c4712f92aa422f830bf47421",
+    "url": "/static/media/ReactHooks.a43fdae0.png"
+  },
+  {
+    "revision": "f2b1b847235119d0ae094a606c74fca4",
+    "url": "/static/media/Spacex.f2b1b847.jpg"
+  },
+  {
+    "revision": "0e12f800206a35a1636a5fa8212c314d",
+    "url": "/static/media/Tetris.0e12f800.jpg"
+  },
+  {
+    "revision": "1c1d4532ab34263efb6aa3a80560326c",
+    "url": "/static/media/Vitech.1c1d4532.png"
+  }
+]);
